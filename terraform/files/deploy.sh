5db8b269bbfa986a69638e3db24cd6329c8b042d
Denis.Tkachev <headsinjars@gmail.com>
2023-11-02 23:03:48 +0500

    HW before *-tasks

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+APP_DIR=${1:-$HOME}
+sudo apt-get install -y git
+sleep 25
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+sleep 25
+cd $APP_DIR/reddit
+bundle install
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
